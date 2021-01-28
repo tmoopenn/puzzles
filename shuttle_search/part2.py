4f21b60a04f0d0696cf5e5553f8a701b234c40ec
Troy Moo Penn <tj-mp@hotmail.com>
2021-01-27 22:20:40 -0500

    add efficient solution shuttle search

--- shuttle_search/part2.py
+++ shuttle_search/part2.py
@@ -1,5 +1,10 @@
-#optimization: use largest bus id as basis for search to increment faster
+from math import gcd 
+
+
 def brute_force(bus_ids):
+    #small optimization: use largest bus id as basis for search to increment faster
+    bus_ids = sorted(bus_ids, key=lambda x: x[0], reverse=True)
+
     t = curr_t = bus_ids[0][0]
     base_index = bus_ids[0][1]
     found_t = False 
@@ -22,16 +27,31 @@ def brute_force(bus_ids):
         m += 1 
     return a_t
 
-#ideas 
-# binary-type search 
-# use largest value as search basis 
+def lcm(nums):
+    lcm = nums[0]
+    for i in range(1, len(nums)):
+        y = nums[i]
+        lcm = (lcm * y) // gcd(lcm, y)
+    return lcm 
+
+def main(bus_ids):
+    aligned = [bus_ids[0][0]]
+    base_index = bus_ids[0][1]
+    timestamp = 0
+    while len(aligned) < len(bus_ids):
+        #print(timestamp)
+        timestamp += lcm(aligned)
+        for id, offset in bus_ids:
+            if id not in aligned:
+                if (timestamp + offset) % id == 0:
+                    aligned.append(id) 
+    return timestamp
 
 if __name__ == "__main__":
-    f = open("sample.txt", "r")
+    f = open("data1.txt", "r")
     earliest_time = int(f.readline().rstrip('\n'))
     ids = f.readline().split(',')
     bus_ids = [(int(id.rstrip('\n')),i) for i,id in enumerate(ids) if id != 'x']
-    bus_ids = sorted(bus_ids, key=lambda x: x[0], reverse=True)
-    print(bus_ids)
-    ans = brute_force(bus_ids)
+    #ans = brute_force(bus_ids)
+    ans = main(bus_ids)
     print(ans)
